769ebeb9fd355fad9f360f735b2c14dba2a69563
MTzenova <1692497@alu.murciaeduca.es>
2025-02-11 16:58:08 +0100

    no funciona el boton de borrar

--- app/(tabs)/bbdd/index.tsx
+++ app/(tabs)/bbdd/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform, View, Text, TextInput, Pressable } from 'react-native';
+import { StyleSheet, Image, Platform, View, Text, TextInput, Pressable, FlatList } from 'react-native';
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -7,7 +7,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import { GlobalStyles } from '@/theme/GlobalStyles';
 import Boton from '@/components/Boton';
-import { addDoc, collection, getDocs, query } from '@firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDocs, query } from '@firebase/firestore';
 import { auth, db } from '@/Firebaseconfig';
 import { useEffect, useState } from 'react';
 
@@ -41,31 +41,41 @@ export default function TabThreeScreen() {
     }
   }
 
+  const borrarTarea = async (id: string) =>{
+    const referenciaTarea = doc(coleccionTareas,id);
+    await (deleteDoc(referenciaTarea));
+    console.log("Tarea eliminada:" + referenciaTarea);
+    cargarDatos();
+  }
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#1D3D47', dark: '#1D3D47' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={GlobalStyles.headerImageBBDD}
-        />
-      }>
+    
       
-      <View style={GlobalStyles.contenedorDesconectar}>
+      <View style={[GlobalStyles.contenedorDesconectar, {marginTop:'10%'}, {marginLeft:'10%'}, {marginRight:'10%'}]}>
         <Text style={GlobalStyles.textoBaseDatos}>Lista de tareas</Text>
         <TextInput
           style={GlobalStyles.input}
-          placeholder="Introduzca una tarea"/>
+          placeholder="Introduzca una tarea" value={tarea} onChangeText={setTarea}/>
         <Pressable style={[GlobalStyles.botonGris, GlobalStyles.boton]} onPress={addTarea}>
           <Text>Add</Text>
         </Pressable>
 
-        <Boton label='Eliminar' backgroundColor='gris' link='../../../'></Boton>
+
+
+       <FlatList data={tareas} keyExtractor={(item) => item.id} renderItem={({item}) => {
+          return (
+            <View>
+              <Text>{item.tarea}</Text>
+              <Pressable style={[GlobalStyles.botonGris, GlobalStyles.boton]}
+              onPress={() => borrarTarea(item.id)}>
+                <Text>Eliminar</Text>
+              </Pressable>
+            </View>
+          )}} />
+
 
       </View>
-      
-    </ParallaxScrollView>
+    
   );
 }
